619497e7a772ad79ff8d2a32a32282ea5c6a50ed
rrockwel <31406562+rrockwel@users.noreply.github.com>
2020-10-05 06:02:26 -0400

    Added form data validation, user feedback messages on validation failure, flex to cards on index page, removed vertical scroll

--- routes/index.js
+++ routes/index.js
@@ -65,41 +65,57 @@ oauth2Client.setCredentials({
 const accessToken = oauth2Client.getAccessToken();
 
 router.post('/contact', (req,res,next)=>{
-	console.log(req.body)
-	// Setup SMTP Server
-	const smtpTrans = nodemailer.createTransport({
-		host: 'smtp.gmail.com',
-		port: 465,
-		secure: true,
-		auth:{
-			type: 'OAuth2',
-			user: 'bestweldingequipment@gmail.com',
-			clientId: '884457304699-jvel3u2q0p2t3gl0tmsps0apnjddreg8.apps.googleusercontent.com',
-			clientSecret: 'k8mkRZCgtdkw1ipgSxniVkyo',
-			refreshToken: '1//04a9PGO_yvLQ6CgYIARAAGAQSNwF-L9Ir1U6UrIEsEOfsGo_yl6t5vbp0uFNtm4PotgbCFCVO4UKwvIQRkXasJEql-w9s4vzO_qY',
-			accessToken: accessToken
-		},
-		logger: true,
-		debug: true
-	})
-
-	// Email Structure
-	const mailOpts = {
-		from: `${req.body.email}`,
-		to: 'bestweldingequipment@gmail.com',
-		subject: 'New User Message from Best Welding Equipment',
-		text: `${req.body.firstName} ${req.body.lastName} (${req.body.email}) says: ${req.body.message}`
+	const { firstName, lastName, email, message } = req.body;
+	let errors = [];
+	if(!firstName){
+		errors.push({ msg : "Please let us know who you are."})
+	} 
+	if(!email){
+		errors.push({ msg : "Please provide your email so we can get back to you."})
 	}
+	if(!message){
+		errors.push({ msg : "Please let us know what you think by including a message." })
+	}
+	if(errors.length>0){
+		res.render('contact', { errors, firstName, lastName, email, message });
+	}else{
 
-	// Attempt to Send Email
-	smtpTrans.sendMail(mailOpts, (error, response)=>{
-		if(response){
-			console.log("response: " + res);
-			res.render('contact-success')
-		}else{
-			console.log("error: " + error);
+		// Setup SMTP Server
+		const smtpTrans = nodemailer.createTransport({
+			host: 'smtp.gmail.com',
+			port: 465,
+			secure: true,
+			auth:{
+				type: 'OAuth2',
+				user: 'bestweldingequipment@gmail.com',
+				clientId: '884457304699-jvel3u2q0p2t3gl0tmsps0apnjddreg8.apps.googleusercontent.com',
+				clientSecret: 'k8mkRZCgtdkw1ipgSxniVkyo',
+				refreshToken: '1//04a9PGO_yvLQ6CgYIARAAGAQSNwF-L9Ir1U6UrIEsEOfsGo_yl6t5vbp0uFNtm4PotgbCFCVO4UKwvIQRkXasJEql-w9s4vzO_qY',
+				accessToken: accessToken
+			},
+			logger: true,
+			debug: true
+		})
+
+		// Email Structure
+		const mailOpts = {
+			from: `${req.body.email}`,
+			to: 'bestweldingequipment@gmail.com',
+			subject: 'New User Message from Best Welding Equipment',
+			text: `${req.body.firstName} ${req.body.lastName} (${req.body.email}) says: ${req.body.message}`
 		}
-	})
+
+		// Attempt to Send Email
+		smtpTrans.sendMail(mailOpts, (error, response)=>{
+			if(response){
+				console.log("response: " + res);
+				res.render('contact-success')
+			}else{
+				console.log("error: " + error);
+			}
+		})
+	}
+	
 })
 
 
