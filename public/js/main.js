ac80d7976106f52750788b6866ae0ea7edef9367
rrockwel <31406562+rrockwel@users.noreply.github.com>
2020-10-12 07:26:05 -0400

    Added MongoURI env variable for connection to db

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,26 @@
 // const navItems = document.querySelectorAll('.menu-nav-item');
 
 
+$(document).ready(()=>{
+
+// Set Session Item firstVisit for once so you can control popup to only happen once
+sessionStorage.setItem('firstVisit', '1');
+
+
 console.log('hello');
 document.addEventListener('mouseout', function(e){
-	if(!e.toElement && !e.relatedTarget){
-		console.log('where are you going?')
+	if((!e.toElement && !e.relatedTarget)&&(sessionStorage.getItem('firstVisit')==='1')){
+		document.getElementById("home-overlay").style.display = 'block';
+		sessionStorage.setItem('firstVisit', '2');
 	}
+})
+
+
+
+
+$('#closeOverlay').on('click', ()=>{
+	document.getElementById('home-overlay').style.display = 'none';
+})
+
+
 })
